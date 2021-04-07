5c1f99d0108147b68db84a2f9aa75410e5d37f46
kamruzzamanripon <rkamruzzaman@gmail.com>
2021-04-08 01:00:47 +0600

    checkout form add

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ class App extends React.Component {
       products : data.products,
       size : "",
       sort : "",
-      cartItems: []
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
     };
+  }
+
+  createOrder = (order) => {
+    alert("Need to save order for" + order.name)
   }
 
   removeFromCart = (product) => {
@@ -20,6 +24,8 @@ class App extends React.Component {
     this.setState({
       cartItems: cartItems.filter((x) => x._id !== product._id)
     })
+
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x) => x._id !== product._id)))
   }
 
   addToCart = (product) =>{
@@ -37,6 +43,7 @@ class App extends React.Component {
     }
 
     this.setState({ cartItems })
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
 
   sortProducts = (event) =>{
@@ -96,6 +103,7 @@ class App extends React.Component {
                     <Cart 
                       cartItems={this.state.cartItems}
                       removeFromCart={this.removeFromCart} 
+                      createOrder={this.createOrder}
                     />
                 </div>
               </div>
